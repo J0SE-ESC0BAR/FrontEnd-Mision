598ab6ed8d868d997bc3ab3dda376f9d770fa4ea
José Alfonso Escobar Mejía <josealfonsoescobarmejia@gmail.com>
2022-03-14 21:03:25 -0600

    Cree mi primera aplicación con Vuejs

---
+++ Ejercicios/VUEJS/app.js
@@ -0,0 +1,26 @@
+const app = Vue.createApp({
+  data() {
+    return {
+      nombre:"Rodrgo",
+      apellido: "Martines",
+      email:"rmartines@innovaccion.mx",
+      genero:"male",
+      foto:""
+    }
+  },
+  methods:{
+    async cambiarUsuario(){
+        const res = await fetch("https://randomuser.me/api");
+        const { results} = await res.json()
+        console.log(results)
+
+
+        this.nombre=results[0].name.first
+        this.apellido=results[0].name.last
+        this.email=results[0].email
+        this.genero=results[0].gender
+        this.foto= results[0].picture.large
+    }
+  }
+})
+app.mount('#app');
